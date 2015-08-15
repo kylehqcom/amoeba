f4c7573e6f0a0f6bad97e7f84cfc67d1d94eac52
Kyle Clarke <git@kylehq.com>
2015-08-15 22:20:16 +1200

    Stopping dupe coordinate calls

--- client/client.js
+++ client/client.js
@@ -1,9 +1,9 @@
 if (Meteor.isClient) {
   // This code only runs on the client
-
   var cells = [];
   for (i = 0; i < 200; i++) {
-    cells.push({ colour: Utils.generateHexColour(), x: Utils.generateRandomCoordinates().x, y: Utils.generateRandomCoordinates().y });
+    var coords = Utils.generateRandomCoordinates();
+    cells.push({ colour: Utils.generateHexColour(), x: coords.x, y: coords.y });
   }
 
   Template.body.helpers({
