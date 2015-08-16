2c4ce259389cf60b0f58d68920c0271fa96001b5
Kyle Clarke <git@kylehq.com>
2015-08-16 21:04:58 +1200

    Introducing a canvas element to work with

--- client/client.js
+++ client/client.js
@@ -1,12 +1,46 @@
 if (Meteor.isClient) {
-  // This code only runs on the client
-  var cells = [];
-  for (i = 0; i < 200; i++) {
-    var coords = Utils.generateRandomCoordinates();
-    cells.push({ colour: Utils.generateHexColour(), x: coords.x, y: coords.y });
-  }
 
-  Template.body.helpers({
-    foodCells: cells
-  });
+    // Draw the canvas
+    Template.canvas.rendered = function() {
+        canvas = $("#canvas");
+        canvas.attr({ "height" : Constants.canvas.height, "width" : Constants.canvas.width });
+        context = canvas[0].getContext("2d");
+
+        // Draw vertical grid lines
+        for (var x = 0.5; x < Constants.canvas.width; x += 10) {
+          context.moveTo(x, 0);
+          context.lineTo(x, Constants.canvas.height);
+        }
+
+        // Draw horizontal grid lines
+        for (var y = 0.5; y < Constants.canvas.height; y += 10) {
+          context.moveTo(0, y);
+          context.lineTo(Constants.canvas.width, y);
+        }
+
+        context.strokeStyle = "#eee";
+        context.stroke();
+
+        context.beginPath();
+        context.moveTo(0, 40);
+        context.lineTo(240, 40);
+        context.moveTo(260, 40);
+        context.lineTo(500, 40);
+        context.moveTo(495, 35);
+        context.lineTo(500, 40);
+        context.lineTo(495, 45);
+
+        context.moveTo(60, 0);
+        context.lineTo(60, 153);
+        context.moveTo(60, 173);
+        context.lineTo(60, 375);
+        context.moveTo(65, 370);
+        context.lineTo(60, 375);
+        context.lineTo(55, 370);
+
+        context.strokeStyle = "#000";
+        context.stroke();
+
+    };
+
 }
