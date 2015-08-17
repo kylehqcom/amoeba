d679507baa6a96710a5e8bdb17c3521f4188bd70
Kyle Clarke <git@kylehq.com>
2015-08-17 18:20:58 +1200

    Adding food cells to the canvas, count should be dictated by collection count value

--- client/canvas.js
+++ client/canvas.js
@@ -1,5 +1,21 @@
 Canvas = {};
 
+Canvas.constants = {
+    height: 400,
+    width: 600,
+    cells: {
+        minimum: 2 // in percent
+    },
+    cell: {
+        radius: 5
+    }
+};
+
+/**
+ * This Canvas elements context
+ */
+var ctx;
+
 /**
  * Initialize a Canvas
  * @argument {String} selector  A css selector to the Canvas element, generally an id
@@ -7,47 +23,59 @@ Canvas = {};
  * @returns  Void
  */
 Canvas.initialize = function (selector, options) {
-
     options = options || {};
-    options.height = options.height || Constants.canvas.height;
-    options.width = options.width || Constants.canvas.width;
+    options.height = options.height || Canvas.constants.height;
+    options.width = options.width || Canvas.constants.width;
 
     canvas = $(selector);
     canvas.attr({ "height" : options.height, "width" : options.width });
-    context = canvas[0].getContext("2d");
+    ctx = canvas[0].getContext("2d");
 
     // Draw vertical grid lines
     for (var x = 0.5; x < options.width; x += 10) {
-      context.moveTo(x, 0);
-      context.lineTo(x, options.height);
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, options.height);
     }
 
     // Draw horizontal grid lines
     for (var y = 0.5; y < options.height; y += 10) {
-      context.moveTo(0, y);
-      context.lineTo(options.width, y);
+      ctx.moveTo(0, y);
+      ctx.lineTo(options.width, y);
     }
+};
 
-    context.strokeStyle = "#eee";
-    context.stroke();
+/**
+ * Based on the stage dimensions, return random x & y points
+ * @returns {Object}
+ */
+Canvas.generateRandomCoordinates = function () {
+    return {
+        x : Random.number(Canvas.constants.width),
+        y : Random.number(Canvas.constants.height)
+    };
+};
 
-    context.beginPath();
-    context.moveTo(0, 40);
-    context.lineTo(240, 40);
-    context.moveTo(260, 40);
-    context.lineTo(500, 40);
-    context.moveTo(495, 35);
-    context.lineTo(500, 40);
-    context.lineTo(495, 45);
+/**
+ * Does this canvas instance have the minimum number of
+ * cells by checking the size of this canvas over the amount of area
+ * covered by cells. Used to pre-load new cell after initialisation.
+ * @returns {Boolean}
+ */
+Canvas.hasMinimumCellCount = function () {
+    // Note i should be the count of a Cells collection
+    i = i || 1;
+    totalArea = Canvas.constants.width * Canvas.constants.height;
+    cellArea  = i * ((this.constants.cell.radius * this.constants.cell.radius) * Math.PI);
+    return (this.constants.cells.minimum < ((cellArea/totalArea) * 100));
+}
 
-    context.moveTo(60, 0);
-    context.lineTo(60, 153);
-    context.moveTo(60, 173);
-    context.lineTo(60, 375);
-    context.moveTo(65, 370);
-    context.lineTo(60, 375);
-    context.lineTo(55, 370);
-
-    context.strokeStyle = "#000";
-    context.stroke();
+/**
+ *
+ */
+Canvas.renderCell = function() {
+    var path = new Path2D(),
+        coords = this.generateRandomCoordinates();
+    path.arc(coords.x, coords.y, this.constants.cell.radius, 0, Utils.degreesToRadians(360));
+    ctx.fillStyle = Colour.generateHexColour();
+    ctx.fill(path);
 };
--- client/client.js
+++ client/client.js
@@ -1,9 +1,24 @@
 if (Meteor.isClient) {
 
-    // Draw the canvas
     Template.canvas.rendered = function() {
+
+        // Initialize the canvas
         Canvas.initialize("#canvas");
 
+        // Check if we have enough cells on the canvas at startup
+        i = 1;
+        while (!Canvas.hasMinimumCellCount()) {
+
+          // Always have a break on while loops!
+          if (i > 1000) {
+              console.log("we broke " + i);
+              break;
+          }
+
+          Canvas.renderCell();
+          console.log(i);
+          i++;
+        }
     };
 
 }
