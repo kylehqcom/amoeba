7bfe8811efbbfa4d121029943f1d45856acac909
Kyle Clarke <git@kylehq.com>
2015-09-01 21:52:57 +1200

    It would help if I stage the files! =]

---
+++ client/templates/actions.js
@@ -0,0 +1,13 @@
+Template.actions.events( {
+    "click .add" : function () {
+        Meteor.call( "cells.insertCell" );
+    },
+
+    "click .increase" : function () {
+        Meteor.call( "cells.insertCell" );
+    },
+
+    "click .remove" : function () {
+        Meteor.call( "cells.deleteCell" );
+    }
+} );
